29a674b2dd490cb873ce1ae46b9d7b737c08a707
ctokoli <matumasie@gamil.com>
2023-06-19 07:18:12 +0000

    fix eslint errors

--- modules/delete.js
+++ modules/delete.js
@@ -2,10 +2,8 @@ import renderBooks from './render.js';
 
 const deleteItem = (id, userData, content) => {
   userData = userData.filter((book) => book.id !== id);
-  console.log(userData.id);
   localStorage.setItem('formdata', JSON.stringify(userData));
   renderBooks(userData, content);
-  console.log(userData);
 };
 
 export default deleteItem;
